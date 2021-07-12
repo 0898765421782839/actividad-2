9d9a9b1fe6f6b9ead7f4add6bb500e12296a2719
0898765421782839 <87339466+0898765421782839@users.noreply.github.com>
2021-07-12 16:29:46 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("\"DANNA GARCÍA\"")
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.TShirt)
+})
 input.onButtonPressed(Button.AB, function () {
     basic.showLeds(`
         . . . . .
